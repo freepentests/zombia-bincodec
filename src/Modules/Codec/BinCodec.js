80e87e559965e099b906472a1459d1fb0bb6abe1
freepentests <252639873+freepentests@users.noreply.github.com>
2026-06-06 09:15:20 +0100

    added most of the opcodes i think

---
+++ src/Modules/Codec/BinCodec.js
@@ -0,0 +1,9 @@
+import Opcodes from './Opcodes.js';
+
+class Encoder {
+}
+
+export default class BinCodec {
+
+}
+
---
+++ src/Modules/Codec/Opcodes.js
@@ -0,0 +1,14 @@
+const Opcodes = {
+	'Input': '1',
+	'JoinGame': '4',
+	'Heartbeat': '7',
+	'Rpc': '9',
+	'1': 'Input',
+	'4': 'JoinGame',
+	'7': 'Heartbeat',
+	'9': 'Rpc'
+};
+
+export default Opcodes;
+
+

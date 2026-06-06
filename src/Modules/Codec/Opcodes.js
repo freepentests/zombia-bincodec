6071f264dba2bea85d1ade8e6e6b6581c3bf1bae
freepentests <252639873+freepentests@users.noreply.github.com>
2026-06-06 09:29:13 +0100

    added entity update opcode

--- src/Modules/Codec/Opcodes.js
+++ src/Modules/Codec/Opcodes.js
@@ -1,8 +1,10 @@
 const Opcodes = {
+	'EntityUpdate': '0',
 	'Input': '1',
 	'JoinGame': '4',
 	'Heartbeat': '7',
 	'Rpc': '9',
+	'0': 'EntityUpdate',
 	'1': 'Input',
 	'4': 'JoinGame',
 	'7': 'Heartbeat',
